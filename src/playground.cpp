89920bbc15f7a286762e0712afae31841a832902
cuda-user-pcc4 <cuda-user-pcc4@lbst-pcc-4-desktop>
2010-05-27 15:19:10 +0200

    Functions for rating and gameIsOver test

--- src/playground.cpp
+++ src/playground.cpp
@@ -16,6 +16,110 @@ Playground::Playground()
 // hat oder 1 für BLACK oder 2 für WHITE.
 int Playground::isGameOver()
 {
+
+	// Vertical staves
+	for(int x=0;x<=3;x++)
+	{
+		for(int y=0;y<=3;y++)
+		{                   
+			if( (get(x, y, 0)!=EMPTY) && (get(x, y, 0)==get(x, y, 1)) && (get(x, y, 1)==get(x, y, 2)) && (get(x, y, 2)==get(x, y, 3)) )
+			{
+				//winner=get(x,y,0);
+				return get(x,y,0);
+			}
+		}
+	}
+
+	// horizontal lines direction: [x]
+	for(int y=0;y<=3;y++)
+		{
+		for(int z=0;z<=3;z++)
+		{                   
+			if( (get(0, y, z)!=EMPTY) && (get(1, y, z)==get(2, y, z)) && (get(2, y, z)==get(3, y, z)) && (get(3, y, z)==get(4, y, z)) )
+			{
+				// winner=get(0, y, z);
+				return get(0, y, z);
+			}
+		}
+	}
+
+	// horizontal lines direction: [y]
+	for(int x=0;x<=3;x++)
+	{
+		for(int z=0;z<=3;z++)
+		{                   
+			if( (get(x, 0, z)!=EMPTY) && (get(x, 0, z)==get(x, 1, z)) && (get(x, 1, z)==get(x, 2, z)) && (get(x, 2, z)==get(x, 3, z)) )
+			{
+				// winner=get(0, y, z);
+				return get(x, 0, z);
+			}
+		}
+	}
+
+	// diagonal bars (low to high)
+	// direction: [x]
+	for(int y=0;y<=3;y++)
+	{
+		if( (get(0,y,0)!=EMPTY) && (get(0,y,0)==get(1,y,1)) && (get(1,y,1)==get(2,y,2)) && (get(2,y,2)==get(3,y,3)) )
+		{ 
+			return get(0,y,0);
+		}
+
+		if( (get(3,y,0)!=EMPTY) && (get(3,y,0)==get(2,y,1)) && (get(2,y,1)==get(1,y,2)) && (get(1,y,2)==get(0,y,3)) )
+		{
+			return get(3,y,0);
+		}
+	}
+
+	// direction: [y] (low to high)
+	for(int x=0;x<=3;x++)
+	{
+		if( (get(x,0,0)!=EMPTY) && (get(x,0,0)==get(x,1,1)) && (get(x,1,1)==get(x,2,2)) && (get(x,2,2)==get(x,3,3)) )
+		{ 
+			return get(x,0,0);
+		}
+
+		if( (get(x,3,0)!=EMPTY) && (get(x,3,0)==get(x,2,1)) && (get(x,2,1)==get(x,1,2)) && (get(x,1,2)==get(x,0,3)) )
+		{
+			return get(x,3,0);
+		}
+	}
+
+	// inner diagonal cross (low to high)
+	if( (get(0,0,0)!=EMPTY) && (get(0,0,0)==get(1,1,1)) &&(get(1,1,1)==get(2,2,2)) && (get(2,2,2)==get(3,3,3)) )
+	{ 
+		return get(0,0,0); // eigentlich besser erst auf 3,3,3 zu testen da unwahrscheinlicher
+	}
+	if( (get(3,3,0)!=EMPTY) && (get(3,3,0)==get(2,2,1)) && (get(2,2,1)==get(1,1,2)) && (get(1,1,2)==get(0,0,3)) )
+	{
+		return get(3,3,0);
+	}
+	    
+	    
+	if( (get(3,0,0)!=EMPTY) && (get(3,0,0)==get(2,1,1)) && (get(2,1,1)==get(1,2,2)) && (get(1,2,2)==get(0,3,3)) )
+	{
+		return get(3,0,0);
+	}
+	if( (get(0,3,0)!=EMPTY) && (get(0,3,0)==get(1,2,1)) && (get(1,2,1)==get(2,1,2)) && (get(2,1,2)==get(3,0,3)) )
+	{
+		return get(0,3,0);
+	}
+	// inner diagonal cross (horizontal bars)
+	for(int z=0;z<=3;z++)
+	{
+	    if( (get(3,3,z)!=EMPTY) && (get(3,3,z)==get(2,2,z)) && (get(2,2,z)==get(1,1,z)) && (get(1,1,z)==get(0,0,z)) )
+	    {
+	       return get(3,3,z);
+	    }    
+	}    
+
+	for(int z=0;z<=3;z++)
+	{
+	    if( (get(0,3,z)!=EMPTY) && (get(0,3,z)==get(1,2,z)) && (get(1,2,z)==get(2,1,z)) && (get(2,1,z)==get(3,0,z)) )
+	    {
+	       return get(0,3,z);
+	    }    
+	} 
 	return 0;
 }
 
@@ -37,7 +141,123 @@ void Playground::set (uint8_t x, uint8_t y, uint8_t z, uint8_t value)
 // Bewertung des aktuellen Spielfeldes
 int Playground::rating(int color)
 {
-	return 1;
+	int ratingVal=0;
+
+	// clone playground 
+
+	// on cloned playground:
+	Playground* tP = this->clone();
+	// fill als empty places with current color
+	for(int x=0;x<=3;x++)
+	{
+		for(int y=0;y<=3;y++)
+		{
+			while(tP->move(x, y));
+		}     
+	}
+
+	// now check how many winning combinations are left 
+		// Vertical staves
+	for(int x=0;x<=3;x++)
+	{
+		for(int y=0;y<=3;y++)
+		{                   
+			if( (get(x, y, 0)!=EMPTY) && (get(x, y, 0)==get(x, y, 1)) && (get(x, y, 1)==get(x, y, 2)) && (get(x, y, 2)==get(x, y, 3)) )
+			{
+				ratingVal++;
+			}
+		}
+	}
+
+	// horizontal lines direction: [x]
+	for(int y=0;y<=3;y++)
+		{
+		for(int z=0;z<=3;z++)
+		{                   
+			if( (get(0, y, z)!=EMPTY) && (get(1, y, z)==get(2, y, z)) && (get(2, y, z)==get(3, y, z)) && (get(3, y, z)==get(4, y, z)) )
+			{
+				ratingVal++;
+			}
+		}
+	}
+
+	// horizontal lines direction: [y]
+	for(int x=0;x<=3;x++)
+	{
+		for(int z=0;z<=3;z++)
+		{                   
+			if( (get(x, 0, z)!=EMPTY) && (get(x, 0, z)==get(x, 1, z)) && (get(x, 1, z)==get(x, 2, z)) && (get(x, 2, z)==get(x, 3, z)) )
+			{
+				ratingVal++;
+			}
+		}
+	}
+
+	// diagonal bars (low to high)
+	// direction: [x]
+	for(int y=0;y<=3;y++)
+	{
+		if( (get(0,y,0)!=EMPTY) && (get(0,y,0)==get(1,y,1)) && (get(1,y,1)==get(2,y,2)) && (get(2,y,2)==get(3,y,3)) )
+		{ 
+			ratingVal++;
+		}
+
+		if( (get(3,y,0)!=EMPTY) && (get(3,y,0)==get(2,y,1)) && (get(2,y,1)==get(1,y,2)) && (get(1,y,2)==get(0,y,3)) )
+		{
+			ratingVal++;
+		}
+	}
+
+	// direction: [y] (low to high)
+	for(int x=0;x<=3;x++)
+	{
+		if( (get(x,0,0)!=EMPTY) && (get(x,0,0)==get(x,1,1)) && (get(x,1,1)==get(x,2,2)) && (get(x,2,2)==get(x,3,3)) )
+		{ 
+			ratingVal++;
+		}
+
+		if( (get(x,3,0)!=EMPTY) && (get(x,3,0)==get(x,2,1)) && (get(x,2,1)==get(x,1,2)) && (get(x,1,2)==get(x,0,3)) )
+		{
+			ratingVal++;
+		}
+	}
+
+	// inner diagonal cross (low to high)
+	if( (get(0,0,0)!=EMPTY) && (get(0,0,0)==get(1,1,1)) && (get(1,1,1)==get(2,2,2)) && (get(2,2,2)==get(3,3,3)) )
+	{ 
+		ratingVal++;
+	}
+	if( (get(3,3,0)!=EMPTY) && (get(3,3,0)==get(2,2,1)) && (get(2,2,1)==get(1,1,2)) && (get(1,1,2)==get(0,0,3)) )
+	{
+		ratingVal++;
+	}
+	    
+	    
+	if( (get(3,0,0)!=EMPTY) && (get(3,0,0)==get(2,1,1)) && (get(2,1,1)==get(1,2,2)) && (get(1,2,2)==get(0,3,3)) )
+	{
+		ratingVal++;
+	}
+	if( (get(0,3,0)!=EMPTY) && (get(0,3,0)==get(1,2,1)) && (get(1,2,1)==get(2,1,2)) && (get(2,1,2)==get(3,0,3)) )
+	{
+		ratingVal++;
+	}
+	// inner diagonal cross (horizontal bars)
+	for(int z=0;z<=3;z++)
+	{
+	    if( (get(3,3,z)!=EMPTY) && (get(3,3,z)==get(2,2,z)) && (get(2,2,z)==get(1,1,z)) && (get(1,1,z)==get(0,0,z)) )
+	    {
+	       ratingVal++;
+	    }    
+	}    
+
+	for(int z=0;z<=3;z++)
+	{
+	    if( (get(0,3,z)!=EMPTY) && (get(0,3,z)==get(1,2,z)) && (get(1,2,z)==get(2,1,z)) && (get(2,1,z)==get(3,0,z)) )
+	    {
+	       ratingVal++;
+	    }    
+	} 
+	return ratingVal;
 }
 
 Playground* Playground::clone()
