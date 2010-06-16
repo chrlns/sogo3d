f3e7bd6e1f9be14059d65f38610db2d62a70022a
Christian Lins <120872+cli@users.noreply.github.com>
2010-06-16 17:39:07 +0200

    Minimax wieder einkommentiert

--- src/gui.cpp
+++ src/gui.cpp
@@ -444,7 +444,7 @@ void mouse(int button, int state, int x, int y)
 	dbgmsg("Klick " << markedStab << " " << x << " " << y);
 	currentPlayground->move(x, y);
 	glutPostRedisplay();
-	//minimax(currentPlayground, BLACK, horizon);
+	minimax(currentPlayground, BLACK, horizon);
 	if(currentPlayground->isGameOver() != EMPTY)
 	{
 		currentPlayground->markWinLine();
